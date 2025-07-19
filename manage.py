a43ecb2ced6d7800ad96839b5920aa6141982b5b
Flass23 <khauhelomakara031@gmail.com>
2025-07-19 00:56:27 -0400

    working on slow performance and db update

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 import os
 from application import create_app, db
+from flask_migrate import Migrate  # type: ignore
 
-from flask_migrate import Migrate   # type: ignore
+# Create the Flask app instance
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+# Optional: Define the shell context
 def make_shell_context():
     return dict(app=app, db=db)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8080 )
-
+    # Added a condition to run the app only when needed
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
